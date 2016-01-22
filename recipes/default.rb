cc8c80d1aeae2b5c142128d14881b36ccb85ff0c
Josh Beauregard <josh.beauregard@knectar.com>
2016-01-22 14:46:49 -0500

    basic package installs

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+
+packages = ['perl', 'tcsh', 'vim', 'nano']
+
+packages.each {|p|
+  # Package resource
+  package p do # Name of the package to install
+    action :install # Install a package - if version is provided, install that specific version (Default)
+  end
+}
+  
