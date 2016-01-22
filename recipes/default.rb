884cd4faa6734b53e5d856d3b86b47c61a758d89
Josh Beauregard <josh.beauregard@knectar.com>
2016-01-22 15:45:25 -0500

    ADD. downloading and untaring

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,12 +5,23 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
 
-packages = ['perl', 'tcsh', 'vim', 'nano']
+packages = ['perl', 'tcsh', 'vim', 'nano', 'tar']
 
 packages.each {|p|
-  # Package resource
-  package p do # Name of the package to install
-    action :install # Install a package - if version is provided, install that specific version (Default)
-  end
+  package p
 }
-  
+
+
+src_filepath  = "#{Chef::Config['file_cache_path'] || '/tmp'}/ureka.tar.gz"
+remote_file 'http://ssb.stsci.edu/ureka/1.5.1/Ureka_linux-rhe6_64_1.5.1.tar.gz' do
+  source 'http://ssb.stsci.edu/ureka/1.5.1/Ureka_linux-rhe6_64_1.5.1.tar.gz'
+  path src_filepath
+end
+
+bash 'unarchive_source' do
+  cwd  ::File.dirname(src_filepath)
+  code <<-EOH
+    tar zxf #{::File.basename(src_filepath)} -C #{::File.dirname(src_filepath)}
+  EOH
+  not_if { ::File.directory?("#{Chef::Config['file_cache_path'] || '/tmp'}/ureka") }
+end
