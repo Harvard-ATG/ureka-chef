0d1ac7abe2b8b189bef3f96a845884d51e3ec3d4
Josh Beauregard <josh.beauregard@knectar.com>
2016-01-27 11:31:02 -0500

    FIX. Fixed install process
    Used Ahttps://hub.docker.com/r/gschnyder/ureka/~/dockerfile/ as a base.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
 
-packages = %w(perl tcsh vim nano tar)
+packages = %w(perl tcsh vim nano tar libx11-6 libXft2 lib32z1 wget bc curl)
 
 packages.each do|p|
   package p
@@ -17,25 +17,26 @@ user 'ureka_user' do
   shell '/bin/tcsh'
 end
 
-# src_filepath  = "#{Chef::Config['file_cache_path'] || '/tmp'}/ureka.tar.gz"
-# remote_file 'http://ssb.stsci.edu/ureka/1.5.1/Ureka_linux-rhe6_64_1.5.1.tar.gz' do
-#  source 'http://ssb.stsci.edu/ureka/1.5.1/Ureka_linux-rhe6_64_1.5.1.tar.gz'
-#  path src_filepath
-# end
-#
-# bash 'unarchive_source' do
-#  cwd  ::File.dirname(src_filepath)
-#  code <<-EOH
-#    tar zxf #{::File.basename(src_filepath)} -C #{::File.dirname(src_filepath)}
-#  EOH
-#  not_if { ::File.directory?("#{Chef::Config['file_cache_path'] || '/tmp'}/ureka") }
-# end
-#
-bash 'install ureka' do
-  user 'root'
-  cwd '/tmp'
-  code <<-EOH
-    curl http://ssb.stsci.edu/ureka/1.5.1/install_ureka_1.5.1 |sh
-  EOH
+src_filepath  = "#{Chef::Config['file_cache_path'] || '/tmp'}/ureka.tar.gz"
+
+remote_file 'http://ssb.stsci.edu/ureka/dev/Ureka_linux-rhe6_64_dev.tar.gz' do
+ source 'http://ssb.stsci.edu/ureka/dev/Ureka_linux-rhe6_64_dev.tar.gz'
+ path src_filepath
 end
 
+bash 'unarchive_source' do
+ cwd  ::File.dirname(src_filepath)
+ code <<-EOH
+   tar -zxf #{::File.basename(src_filepath)} -C #{::File.dirname(src_filepath)}
+   Ureka/bin/ur_normalize -s
+ EOH
+ not_if { ::File.directory?("#{Chef::Config['file_cache_path'] || '/tmp'}/ureka") }
+end
+
+bash 'run_ureka_setup' do
+  cwd '/root'
+  code <<-EOH
+    ["/bin/bash", "-c", "echo \"ur_setup common primary\" >> .profile"] 
+    ["/bin/bash", "-c", "source .profile"]
+  EOH
+end
